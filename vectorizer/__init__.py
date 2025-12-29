f7298c0462bbbbed68868db2e6c5941ef189f659
hosamreda <89112659+hosamreda@users.noreply.github.com>
2025-12-29 10:22:57 +0200

    Add image vectorization CLI and pipeline

---
+++ vectorizer/__init__.py
@@ -0,0 +1,8 @@
+from .pipeline import VectorizationError, VectorizationReport, VectorizationSettings, vectorize_image
+
+__all__ = [
+    "VectorizationError",
+    "VectorizationReport",
+    "VectorizationSettings",
+    "vectorize_image",
+]
